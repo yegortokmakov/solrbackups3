1339a10e66b0266008aff77a8b59beec104ac33e
Yegor Tokmakov <yegor.tokmakov@home24.de>
2015-05-11 17:37:34 +0200

    restore

--- solrbackups3.py
+++ solrbackups3.py
@@ -1,48 +1,125 @@
 #!/usr/bin/python
 
 import argparse
-import glob
 import os
 import tarfile
-import urllib2
 import time
-import json
-import sys
 import shutil
-from subprocess import check_call
+import subprocess
+import sys
 
-parser = argparse.ArgumentParser(description='Backup Solr to S3')
-parser.add_argument('--host', dest='host', default='localhost')
-parser.add_argument('--port', dest='port', default='8983')
-parser.add_argument('--location', dest='location', default='/tmp')
-parser.add_argument('--bucket', dest='bucket', default='h24-backup-solr')
 
-args = parser.parse_args()
+def backup(args):
+    snapshot_directory = "%s/snapshot.%s" % (args.location, time.time())
+    os.mkdir(snapshot_directory)
+    print 'Snapshot directory created: %s' % snapshot_directory
 
-snapshot_directory = "%s/snapshot.%s" % (args.location, time.time())
-os.mkdir(snapshot_directory)
-print 'Snapshot directory created: %s' % snapshot_directory
+    print 'Backup start...'
+    subprocess.check_call(["python", "src/solrbackup-master/solrbackup/solrbackup.py", "http://%s:%s/solr" % (args.host, args.port), snapshot_directory])
 
-print 'Backup start...'
-check_call(["python", "src/solrbackup/solrbackup/solrbackup.py", "http://%s:%s/solr" % (args.host, args.port), snapshot_directory])
+    archive_name = '%s/solr.%s.tgz' % (args.location, time.strftime("%Y%m%d.%H%M%S"))
+    print 'Archiving to %s' % archive_name
 
-archive_name = '%s/solr.%s.tgz' % (args.location, time.strftime("%Y%m%d.%H%M%S"))
-print 'Archiving to %s' % archive_name
+    with tarfile.open(archive_name, "w:gz") as tar:
+        for dirname, dirnames, filenames in os.walk(snapshot_directory):
+            for subdirname in dirnames:
+                tar.add(os.path.join(dirname, subdirname), arcname=subdirname)
+                print(subdirname)
 
-with tarfile.open(archive_name, "w:gz") as tar:
-    # tar.add(snapshot_directory)
+    print 'Uploading to AWS'
+    subprocess.check_call(["aws", "s3", "cp", archive_name, "s3://" + args.bucket])
+
+    print 'Removing snapshot directory.'
+    shutil.rmtree(snapshot_directory)
+
+    print 'Removing backup archvie'
+    os.remove(archive_name)
+
+    print '+++ done'
+
+
+def restore(args):
+    print 'Downloading backup'
+    subprocess.check_call(["aws", "s3", "cp", "s3://" + args.filename, args.location])
+
+    snapshot_directory = "%s/snapshot.%s" % (args.location, time.time())
+    os.mkdir(snapshot_directory)
+
+    print 'Extracting files to: %s' % snapshot_directory
+    tar = tarfile.open("%s/%s" % (args.location, args.filename))
+    tar.extractall(path=snapshot_directory)
+    tar.close()
+
+    print 'Stopping Solr'
+    subprocess.check_call(["sudo", "service", "solr", "stop"])
+
+    process = os.popen("sudo service solr status | grep uptime").read()
+    if process:
+        print 'Solr running! terminate...'
+        sys.exit()
+
     for dirname, dirnames, filenames in os.walk(snapshot_directory):
         for subdirname in dirnames:
-            tar.add(os.path.join(dirname, subdirname), arcname=subdirname)
-            print(subdirname)
+            print 'Restoring core: %s' % subdirname
 
-print 'Uploading to AWS'
-check_call(["aws", "s3", "cp", archive_name, "s3://" + args.bucket])
+            print '--- Removing transaction log'
+            for root, dirs, files in os.walk("%s/%s/data/tlog" % (args.solrpath, subdirname)):
+                for f in files:
+                    os.unlink(os.path.join(root, f))
+                for d in dirs:
+                    shutil.rmtree(os.path.join(root, d))
 
-print 'Removing snapshot directory.'
-shutil.rmtree(snapshot_directory)
+            print '--- Removing index data'
+            for root, dirs, files in os.walk("%s/%s/data/index" % (args.solrpath, subdirname)):
+                for f in files:
+                    os.unlink(os.path.join(root, f))
+                for d in dirs:
+                    shutil.rmtree(os.path.join(root, d))
 
-print 'Removing backup archvie'
-os.remove(archive_name)
+            print '--- Restoring index data'
+            for root, dirs, files in os.walk(snapshot_directory):
+                for f in files:
+                    shutil.copy(os.path.join(root, f), "%s/%s/data/index/" % (args.solrpath, subdirname))
 
-print '+++ done'
+    print 'Starting Solr'
+    subprocess.check_call(["sudo", "service", "solr", "start"])
+
+    print 'Removing snapshot directory.'
+    shutil.rmtree(snapshot_directory)
+
+    print 'Removing backup archvie'
+    os.remove("%s/%s" % (args.location, args.filename))
+
+    print '+++ done'
+
+def list(args):
+    print 'List of available backups'
+    subprocess.check_call(["aws", "s3", "ls", "s3://" + args.bucket])
+
+def main():
+    parser = argparse.ArgumentParser(description='Backup Solr to S3', add_help=False)
+
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    parser_backup = subparsers.add_parser('backup', help='a help')
+    parser_backup.add_argument('bucket', help='S3 bucket')
+    parser_backup.add_argument('--host', dest='host', default='localhost')
+    parser_backup.add_argument('--port', dest='port', default='8983')
+    parser_backup.add_argument('--location', dest='location', default='/tmp')
+    parser_backup.set_defaults(func=backup)
+
+    parser_restore = subparsers.add_parser('restore', help='a help')
+    parser_restore.add_argument('bucket', help='S3 bucket')
+    parser_restore.add_argument('filename', help='Remote filename to restore')
+    parser_restore.add_argument('--location', dest='location', default='/tmp')
+    parser_restore.add_argument('--solrpath', dest='solrpath', default='/var/solr/data')
+    parser_restore.set_defaults(func=restore)
+
+    parser_list = subparsers.add_parser('list', help='List available backups')
+    parser_list.add_argument('bucket', help='S3 bucket')
+    parser_list.set_defaults(func=list)
+
+    args = parser.parse_args()
+    args.func(args)
+
+if __name__ == '__main__': main()
